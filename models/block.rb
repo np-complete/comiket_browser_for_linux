6438986e80a48b5e0dba7c915038e074b067e81c
masarakki <masaki@hisme.net>
2013-01-13 17:01:41 +0900

    add connection methods to Block

--- models/block.rb
+++ models/block.rb
@@ -1,4 +1,19 @@
 class Block < ActiveRecord::Base
+  attr_accessor :day
   attr_accessible :name
+
   validates :name, uniqueness: true
+
+  def next_block_id(day = nil)
+    circles = Circle.where(day: day || @day, comiket_no: comiket_no).where("block_id > ?", id).order("block_id asc").limit(1)
+    return nil if circles.count == 0
+    circles.first.block_id
+  end
+
+  def prev_block_id(day = nil)
+    circles = Circle.where(day: day || @day, comiket_no: comiket_no).where("block_id < ?", id).order("block_id desc").limit(1)
+    return nil if circles.count == 0
+    circles.first.block_id
+  end
+
 end
--- spec/factories/blocks.rb
+++ spec/factories/blocks.rb
@@ -2,5 +2,9 @@ FactoryGirl.define do
   factory :block do
     comiket_no Comiket::No
     sequence(:name) { |n| "A" + n.to_s }
+
+    trait :old do
+      comiket_no Comiket::No - 1
+    end
   end
 end
