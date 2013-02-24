452e518a62dcb9db59defc6e3479d02f92b947aa
masarakki <masaki@hisme.net>
2013-02-25 08:40:48 +0900

    fix search prev block

--- models/block.rb
+++ models/block.rb
@@ -4,16 +4,29 @@ class Block < ActiveRecord::Base
 
   validates :name, uniqueness: true
 
-  def next_block_id(day = nil)
-    circles = Circle.where(day: day || @day, comiket_no: comiket_no).where("block_id > ?", id).order("block_id asc").limit(1)
-    return nil if circles.count == 0
-    circles.first.block_id
+  def self.find_and_day(id, day, comiket_no = nil)
+    block = Block.find(id)
+    block.day = day
+    block
   end
 
-  def prev_block_id(day = nil)
-    circles = Circle.where(day: day || @day, comiket_no: comiket_no).where("block_id < ?", id).order("block_id desc").limit(1)
-    return nil if circles.count == 0
-    circles.first.block_id
+  def next_block
+    @next_block ||= begin
+                      circles = Circle.where(day: day || @day, comiket_no: comiket_no).where("block_id > ?", id).order("block_id asc").limit(1)
+                      return nil if circles.count == 0
+                      Block.find_and_day(circles.first.block_id, day)
+                    end
   end
 
+  def prev_block
+    @prev_block ||= begin
+                      circles = Circle.where(day: day || @day, comiket_no: comiket_no).where("block_id < ?", id).order("block_id desc").limit(1)
+                      return nil if circles.count == 0
+                      Block.find_and_day(circles.first.block_id, day)
+                    end
+  end
+
+  def pages
+    (Circle.where(day: day || @day, comiket_no: comiket_no, block_id: id).count.to_f / 16).ceil
+  end
 end
--- spec/models/block_spec.rb
+++ spec/models/block_spec.rb
@@ -4,11 +4,15 @@ describe "Block Model" do
   let(:block) { FactoryGirl.create(:block, :day => 1) }
 
   describe :accessor do
+    before { FactoryGirl.create_list(:circle, 60, day: 1, block: block) }
+
     subject { block }
     its(:day) { should == 1 }
+    its(:pages) { should == 4 }
   end
 
   describe :connections do
+    subject { blocks[2] }
     let(:blocks) { FactoryGirl.create_list(:block, 5) }
     let!(:circles) do
       [0, 1, 2, 3, 4].map do |i|
@@ -19,24 +23,24 @@ describe "Block Model" do
 
     context :last_block do
       subject { blocks[4] }
-      its(:next_block_id) { should == nil }
+      its(:next_block) { should == nil }
     end
 
     context :first_block do
-      its(:prev_block_id) { should == nil }
+      subject { blocks[0] }
+      its(:prev_block) { should == nil }
     end
 
-    subject { blocks[2] }
-    context :day_1 do
+    context :not_skipped_block do
       before { blocks[2].day = 1 }
-      its(:prev_block_id) { should == blocks[1].id }
-      its(:next_block_id) { should == blocks[3].id }
+      its(:prev_block) { should == blocks[1] }
+      its(:next_block) { should == blocks[3] }
     end
 
-    context :day_2 do
+    context :skipped_block do
       before { blocks[2].day = 2 }
-      its(:prev_block_id) { should == blocks[0].id }
-      its(:next_block_id) { should == blocks[4].id }
+      its(:prev_block) { should == blocks[0] }
+      its(:next_block) { should == blocks[4] }
     end
   end
 end
